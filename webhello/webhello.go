0a7ece16ced6bec5ecc041fc6c06074b4ee0716a
Sathya Thilakan <sthilakan@eyeota.com>
2014-12-13 22:01:56 +0800

    print message in console

--- webhello/webhello.go
+++ webhello/webhello.go
@@ -12,5 +12,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	port := ":8080"
+	fmt.Println("Listening in port ", port)
+	http.ListenAndServe(port, nil)
 }
